015b7759074c2b67fb21a34dfe8b5a99c8365a0c
Adeyemi Damilola <adeyemid266@gmail.com>
2024-09-30 15:07:38 +0100

    changes

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,18 +1,15 @@
 "use client";
 
-import axios from "axios";
 import React, { useEffect, useState } from "react";
-import Chart from "chart.js/auto";
+import MyTable from "@/components/MyTable";
+import * as axiosHandler from "@/handlers/axiosHandler";
 import {
   makeFirstCharUpper,
   makeMonetaryNumber,
 } from "@/handlers/helperHandler";
-import MyTable from "@/components/MyTable";
-import * as axiosHandler from "@/handlers/axiosHandler";
 
-const page = () => {
+const Page = () => {
   const [stats, setStats] = useState([]);
-
   const [users, setUsers] = useState([]);
   const [transactions, setTransactions] = useState([]);
 
@@ -59,17 +56,26 @@ const page = () => {
     fetchTransactionData();
   }, []);
 
+  const backgroundColors = [
+    "bg-[#4A90E2]", // Color for Total Users
+    "bg-[#EA6A47]", // Color for Total Transactions
+    "bg-[#F5A623]", // Color for Total Wallet Balance
+  ];
+
+  const bordercolor = [
+    "border-[#4A90E2]", // Color for Total Users
+    "border-[#EA6A47]", // Color for Total Transactions
+    "border-[#F5A623]", // Color for Total Wallet Balance
+  ];
   return (
     <div className="flex flex-col items-start justify-start pt-20 self-stretch w-full">
       <div className="w-full flex flex-col lg:flex-row items-center justify-center gap-4">
-        {" "}
-        {}
         {stats.map((stat, statKey) => (
           <div
-            className="w-full lg:w-2/12 p-3 flex flex-grow flex-col gap-2 mb-8 bg-[#EA6A47] text-white shadow-lg rounded-lg border border-[#EA6A47]"
+            className={`w-full lg:w-2/12 p-3 flex flex-grow flex-col gap-2 mb-8 ${backgroundColors[statKey]} text-white shadow-lg rounded-lg border ${bordercolor[statKey]}`}
             key={statKey}
           >
-            <span className="text-2xl font-extrabold"> {stat.title} </span>
+            <span className="text-2xl font-extrabold">{stat.title}</span>
             <span className="text-5xl font-extralight">{stat.count}</span>
           </div>
         ))}
@@ -79,13 +85,11 @@ const page = () => {
           <MyTable
             title={"Top Active Users"}
             data={
-              users?.map((user) => {
-                return [
-                  makeFirstCharUpper(user.full_name, " "),
-                  makeMonetaryNumber(user.total_wallet_balance),
-                  user.status,
-                ];
-              }) ?? []
+              users?.map((user) => [
+                makeFirstCharUpper(user.full_name, " "),
+                makeMonetaryNumber(user.total_wallet_balance),
+                user.status,
+              ]) ?? []
             }
             keys={["Account Name", "Wallet Balance", "Account Status"]}
             spacing={"w-1/3 lg:w-1/4"}
@@ -95,14 +99,12 @@ const page = () => {
           <MyTable
             title={"Recent Transactions"}
             data={
-              transactions?.map((transaction) => {
-                return [
-                  makeFirstCharUpper(transaction.user.full_name, " "),
-                  makeFirstCharUpper(transaction.type),
-                  makeMonetaryNumber(transaction.total_amount),
-                  makeFirstCharUpper(transaction.status),
-                ];
-              }) ?? []
+              transactions?.map((transaction) => [
+                makeFirstCharUpper(transaction.user.full_name, " "),
+                makeFirstCharUpper(transaction.type),
+                makeMonetaryNumber(transaction.total_amount),
+                makeFirstCharUpper(transaction.status),
+              ]) ?? []
             }
             keys={["Account Name", "Type", "Amount", "Transaction Status"]}
             spacing={"w-1/3 lg:w-1/5"}
@@ -113,4 +115,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
