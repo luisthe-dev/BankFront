e2bf9bca907baa848b1ddd1b885c2aa458fb3eb7
Adesiyan Divine <divinegiftadesiyan@gmail.com>
2024-10-15 10:00:41 +0100

    fix breaks

--- src/components/UserDashboard/Header.jsx
+++ src/components/UserDashboard/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { RiMenu5Fill } from "react-icons/ri";
 import { FiUser } from "react-icons/fi";
+import Image from "next/image";
 
 const Header = ({ menuHandler, menuState }) => {
   return (
