3211fe233692291adac16811598b3806088ea870
Adesiyan Divine <divinegiftadesiyan@gmail.com>
2024-12-11 19:06:18 +0100

    update url

--- src/components/SiteHeader.jsx
+++ src/components/SiteHeader.jsx
@@ -1,15 +1,20 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 import Logo from "@/assets/images/logo.png";
 import { RxArrowTopRight } from "react-icons/rx";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const SiteHeader = () => {
+  const router = useRouter();
+
   return (
     <>
       <nav className="flex justify-between flex-row items-center w-full p-12">
         <div className="flex justify-center items-start w-6/12 lg:w-1/12">
-          <Image src={Logo} alt={"Logo"} />
+          <Image src={Logo} alt={"Logo"} onClick={() => router.push("/")} />
         </div>
         <div className="flex flex-row justify-end items-center gap-8 flex-grow">
           <Link
